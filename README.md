83bffdd2356067cd6da20ad7d654ba4dedecd658
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2018-04-20 17:26:32 -0500

    feat: restructured and cleaned

--- README.md
+++ README.md
@@ -1,95 +1,35 @@
 # sublime-react-es6
 
-This is a fork of the fork (forkception) of Facebook's original ReactJS Sublime Package 
+This is a fork of NicoSantangelo/sublime-react-es6 Sublime Package 
 
-The main changes include:
-
-- Supporting [standard](https://github.com/feross/standard)
-
-- Converting function declarations to the new ES6 shorthand.
-
-- Static class variables (defaultProps, propTypes) are declared using ES7 property intializers.
-
-- ES6 style exports and imports for component creating snippets.
-
-- Added `rrc` for Redux connected components.
-
-![alt tag](https://raw.githubusercontent.com/mboperator/sublime-react/master/docs/img/sr-rcc-out.gif)
+The goal is to keep it fresh and update to date to how Curtis' uses React
 
 ## Installation
 
-Install the React package via Sublime's Package Manager
-
-You will need the Sublime [Package Manager](https://sublime.wbond.net/installation).
-
-- Open the command palette: `⌘+shift+p` on MacOS/Linux, `ctrl+shift+p` on Windows
-
-- type `install`, select `Package Control: Install Package`
-
-- type `React`, select `React ES6 Snippets`
+Install the React package via Sublime's Package Manager via url
+```https://github.com/CurtisHumphrey/sublime-react-es6.git```
 
 ## Usage
 
-### Syntax highlighting
-
-*Syntax highlighting is no longer provided by this packages*. We recommend that you use ([babel-sublime](https://github.com/babel/babel-sublime)) instead.
-
-### Snippets
-
 It's easy! Simply activate snippets by typing a mnemonic followed by TAB.
 
-![alt tag](https://raw.githubusercontent.com/mboperator/sublime-react/master/docs/img/sr-snippets-out.gif)
-
-#### Documentation of available snippets (JSX):
-
+### Documentation of available snippets:
 ```
-    cdm→  componentDidMount: fn() { ... }
 
-   cdup→  componentDidUpdate: fn(pp, ps) { ... }
+    cdm→  componentDidMount: fn () { ... }
 
-     cs→  import cx from 'classnames';
+   cwun→  componentWillUnmount: fn () { ... }
 
-    cwm→  componentWillMount: fn() { ... }
+    scu→  shouldComponentUpdate: fn (np, ns) { ... }
 
-    cwr→  componentWillReceiveProps: fn(np) { ... }
+    sdp→  static defaultProps = { ... } 
 
-    cwu→  componentWillUpdate: fn(np, ns) { ... }
-
-   cwun→  componentWillUnmount: fn() { ... }
-
-     cx→  cx({ ... })
-
-    fdn→  React.findDOMNode(...)
-
-    fup→  forceUpdate(...)
-
-    gdp→  static defaultProps = { ... } 
-
-    gis→  getInitialState: fn() { return {...} } 
-
-  props→  this.props.
-
-     pt→  propTypes { ... }
-
-    rcc→  component skeleton
-
-   refs→  this.refs.
-
-    ren→  render: fn() { return ... }
-
-    rrc→  redux component skeleton
-
-    scu→  shouldComponentUpdate: fn(np, ns) { ... }
-
-    sst→  this.setState({ ... })
+    spt→  static propTypes { ... }
 
   state→  this.state.
 
+     tp→  this.props.
+
+    tst→  this.setState({ ... })
+
 ```
-
-## Contributing
-
-### Rebuilding the docs
-
-After making changes to snippet files, run `npm install && npm run build-docs` to automatically generate this document from source. **Do not** make changes to README.md directly.
-
--- generateReadme.js
+++ generateReadme.js
@@ -1,75 +1,28 @@
 var fs = require('fs');
-var async = require('async');
 
-var DOCUMENTATION_TOP =
-  '# sublime-react-es6\n\n' +
-  'This is a fork of Facebook\'s original ReactJS Sublime Package.\n\n'+
-  'The main changes I\'ve made include:\n\n' +
-  '- Converting function declarations to the new ES6 shorthand.\n\n'+
-  '- Static class variables (defaultProps, propTypes) are declared using ES7 property intializers.\n\n'+
-  '- ES6 style exports and imports for component creating snippets.\n\n'+
-  '- Added `rrc` for Redux connected components.\n\n'+
-  '![alt tag](https://raw.githubusercontent.com/mboperator/sublime-react/master/docs/img/sr-rcc-out.gif)\n\n' +
-  '## Installation\n\n' +
-  'Install the React package via Sublime\'s Package Manager\n\n' +
-  'You will need the Sublime [Package Manager](https://sublime.wbond.net/installation).\n\n' +
-  '- Open the command palette: `⌘+shift+p` on MacOS/Linux, `ctrl+shift+p` on Windows\n\n' +
-  '- type `install`, select `Package Control: Install Package`\n\n' +
-  '- type `React`, select `React ES6 Snippets`\n\n' +
-  '## Usage\n\n' +
-  '### Syntax highlighting\n\n' +
-  '*Syntax highlighting is no longer provided by this packages*. We recommend that you use ([babel-sublime](https://github.com/babel/babel-sublime)) instead.\n\n' +
-  '### Snippets\n\n' +
-  'It\'s easy! Simply activate snippets by typing a mnemonic followed by TAB.\n\n' +
-  '![alt tag](https://raw.githubusercontent.com/mboperator/sublime-react/master/docs/img/sr-snippets-out.gif)\n\n' +
-  '#### Documentation of available snippets (JSX):\n\n' +
-  '```\n';
+const first_break_point = '### Documentation of available snippets:'
+const second_break_point = '```'
 
-var DOCUMENTATION_BOTTOM =
-  '```\n\n' +
-  '## Contributing\n\n' +
-  '### Rebuilding the docs\n\n' +
-  'After making changes to snippet files, run `npm install && npm run build-docs` to automatically generate this document from source. **Do not** make changes to README.md directly.\n\n';
+const readme_file = fs.readFileSync('./README.md', 'utf-8')
 
-fs.readdir('./snippets/js', function(err, files) {
-  var snippets = files.filter(function(file) {
-    return file.substr(-16) === '.sublime-snippet';
-  }).map(function(file) {
-    return './snippets/js/' + file;
-  });
-  async.map(snippets, readAndInspect, function(err, results) {
-    if (err) {
-      console.error('error mapping snippets', err);
-    }
-    var snippetDocs =
-      DOCUMENTATION_TOP +
-      results.map(function(snippet) {
-        return inspectFile(snippet);
-      }).sort(function(a, b) {
-        return a.abbreviation > b.abbreviation
-          ? 1
-          : a.abbreviation === b.abbreviation
-            ? 0
-            : -1;
-      }).map(function(snippet) {
-        return snippet.docBlock;
-      }).join('') +
-      DOCUMENTATION_BOTTOM;
-    fs.writeFile('README.md', snippetDocs, function (err) {
-      if (err) {
-        console.error('error appending README:', err);
-      }
-    });
-  });
-});
+let parts = readme_file.split(first_break_point)
+const header = parts[0] + first_break_point
+parts = parts[1].split(second_break_point)
+const footer = parts[parts.length - 1]
 
-function readAndInspect(fileName, cb) {
-  fs.readFile(fileName, 'utf-8', function(err, contents) {
-    if (!err) {
-      cb(null, contents);
-    }
-  });
-}
+let files = fs.readdirSync('./snippets').filter((file) => file.substr(-16) === '.sublime-snippet')
+
+let snippets_docs = files.map((filename) => {
+  const file = fs.readFileSync(`./snippets/${filename}`, 'utf-8')
+  return inspectFile(file)
+})
+snippets_docs.sort((a, b) => a.abbreviation > b.abbreviation ? 1 : (a.abbreviation === b.abbreviation) ? 0 : -1)
+
+snippets_docs = snippets_docs.map((snippet) => snippet.docBlock).join('')
+
+const readme = header + '\n```\n\n' + snippets_docs + '```' + footer
+
+fs.writeFileSync('README.md', readme)
 
 function inspectFile(contents) {
   var match = contents.match(
